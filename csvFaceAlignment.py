afc72cc1e6a1c7fb320c3e1171227cdb61ae021f
estranho <marcoantonio.maas@gmail.com>
2014-03-11 04:38:23 -0300

    Minor fixes

--- csvFaceAlignment.py
+++ csvFaceAlignment.py
@@ -12,7 +12,10 @@ reader = csv.reader(ifile)
 
 for row in reader:
     path = sys.argv[1].replace(sys.argv[1].split('/')[-1],'')
-    img = pv.Image(path+row[0])
+    try:
+        img = pv.Image(path+row[0])
+    except:
+        continue
     #cols = temp.shape[0]
     #rows = temp.shape[1]        
     #im = np.ones((rows+200,cols+200),dtype = np.float32)
@@ -21,6 +24,8 @@ for row in reader:
     #im = pv.Image(im)
     #im = pv.Image(im.asPIL())
     
+    left_eye = pv.Point(float(row[1]),float(row[2]))
+    right_eye = pv.Point(float(row[3]),float(row[4]))
     center_of_eyes = pv.Point((float(row[1])+float(row[3]))/2,(float(row[2])+float(row[4]))/2)
     mouth = pv.Point(float(row[5]),float(row[6]))
 
